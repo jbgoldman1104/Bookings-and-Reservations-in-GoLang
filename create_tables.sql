3367f372c580011910272c816a3943583823caf0
jbgoldman1104 <jbgoldman1104@gmail.com>
2023-05-16 18:45:38 +0900

    "updated on 2023-05-16"

--- create_tables.sql
+++ create_tables.sql
@@ -3,12 +3,6 @@
 --
 
 -- Dumped from database version 14.5 (Homebrew)
--- Dumped by pg_dump version 14.5 (Homebrew)
-
-SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET idle_in_transaction_session_timeout = 0;
-SET client_encoding = 'UTF8';
 SET standard_conforming_strings = on;
 SELECT pg_catalog.set_config('search_path', '', false);
 SET check_function_bodies = false;
